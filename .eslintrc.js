205411e88b11ea7abebb7f41e43345c618168599
Nick Meldrum <nick@nickmeldrum.com>
2018-11-07 16:28:34 +0000

    add basic dynamo feature

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'no-console': 0,
+  },
+}
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-var ApiBuilder = require('claudia-api-builder'),
-  api = new ApiBuilder();
+const ApiBuilder = require('claudia-api-builder')
+const AWS = require('aws-sdk')
 
-module.exports = api;
+const api = new ApiBuilder()
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-api.get('/hello', function () {
-  return 'hello world';
-});
+api.get('/health', () => ({
+  status: 'a-okay',
+})
+
+api.get('/test', request => JSON.stringify(request))
+
+api.get('/emails', request => {
+  return dynamoDb.scan({ TableName: 'emails' })
+    .promise()
+    .then(response => response.Items)
+})
+
+api.post('/emails', req => {
+  const params = {
+    TableName: 'emails',
+    Item: {
+        site: req.body.site,
+        email: req.body.email,
+    },
+  }
+  return dynamoDb.put(params).promise()
+})
+
+module.exports = api
