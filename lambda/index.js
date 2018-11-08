8d9fee6867f652d10b853ccd844c47d2ff331e5f
Nick Meldrum <nick@nickmeldrum.com>
2018-11-08 16:28:16 +0000

    add cors support

--- lambda/index.js
+++ lambda/index.js
@@ -1,29 +1,29 @@
 const ApiBuilder = require('claudia-api-builder')
 const AWS = require('aws-sdk')
-const responses = require('./lib/responses')
+const response = require('./lib/responses')
 const siteAllowed = require('./lib/sites').siteAllowed
 
 const api = new ApiBuilder()
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-api.get('/health', () => ({ status: 'a-okay', }))
+api.get('/health', () => response.success('a-okay'))
 
-api.get('/emails', req => responses.serverError('not implemented'))
+api.get('/emails', req => response.serverError('not implemented'))
 
-const validateInput = body => {
-  if (!body.site) return responses.badRequest('body must include site')
-  if (!siteAllowed(body.site)) return responses.badRequest('site not registered, sign up at https://www.emailcollect.com!')
+const validateInput = (body) => {
+  if (!body.site) return response.badRequest('body must include site')
+  if (!siteAllowed(body.site)) return response.badRequest('site not registered, sign up at https://www.emailcollect.com!')
 
   const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
-  if (!body.email) return responses.badRequest('body must include email')
-  if (!body.email.includes('@')) return responses.badRequest('email must include an @')
-  if (!re.test(body.email.trim())) return responses.badRequest('email is not valid')
+  if (!body.email) return response.badRequest('body must include email')
+  if (!body.email.includes('@')) return response.badRequest('email must include an @')
+  if (!re.test(body.email.trim())) return response.badRequest('email is not valid')
   
   // TODO - validate site of email AND sourceIp AND userAgent to stop crazy big objects getting into dynamo
   // TODO - how to lock endpoint down to stop hackers spamming me?
   // TODO - what about stopping silly email addresses?
   // what about stopping duplicates - currently overwrites?
-    // check the referrer is from valid domain as well
+  // check the referrer is from valid domain as well
 
   return false
 }
@@ -40,11 +40,18 @@ const updateDynamoDb = ({ body: {site, email}, context: {sourceIp, userAgent} })
     },
   }).promise()
 
+api.corsOrigin(req => {
+  if (!req.headers.origin) return ''
+  const origin = req.headers.origin.replace('https://', '')
+  return siteAllowed(origin) ? req.headers.origin : ''
+})
+
 api.post('/emails', req => {
   const validationError = validateInput(req.body)
   if (validationError) return validationError
 
-  return updateDynamoDb(req)
+  return updateDynamoDb(req).then(result => response.success('subscribed'))
 })
+
 
 module.exports = api
--- lambda/lib/responses.js
+++ lambda/lib/responses.js
@@ -1,12 +1,19 @@
 const ApiBuilder = require('claudia-api-builder')
 
-const customResponse = statusCode => error => new ApiBuilder.ApiResponse(
-  { error },
-  {'Content-Type': 'application/json'},
-  statusCode,
-)
+const customResponse = statusCode => body => {
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+
+  return new ApiBuilder.ApiResponse(
+    (typeof body === 'string') ? { info: body } : body,
+    headers,
+    statusCode,
+  )
+}
 
 module.exports = {
+  success: customResponse(200),
   badRequest: customResponse(400),
   serverError: customResponse(500),
 }
