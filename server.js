b2f52d3ab05ce7caa25b2dc57d97833a8707cdc6
Nick Meldrum <nick@nickmeldrum.com>
2018-11-08 08:47:09 +0000

    add initial cf template for dynamo creation

--- server.js
+++ server.js
@@ -4,24 +4,33 @@ const AWS = require('aws-sdk')
 const api = new ApiBuilder()
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-api.get('/health', () => ({
-  status: 'a-okay',
-})
+api.get('/health', () => ({ status: 'a-okay', }))
 
-api.get('/test', request => JSON.stringify(request))
-
-api.get('/emails', request => {
+api.get('/emails', req => new ApiBuilder.ApiResponse(
+  { something: 'booyah'},
+  {'Content-Type': 'application/json'},
+  400,
+))
+  /*
   return dynamoDb.scan({ TableName: 'emails' })
     .promise()
     .then(response => response.Items)
-})
+  */
 
 api.post('/emails', req => {
+  const email = req.body.email.trim()
+  /*
+  if (email.includes('@'))
+    return
+  */
   const params = {
     TableName: 'emails',
     Item: {
         site: req.body.site,
-        email: req.body.email,
+        email: email,
+        sourceIp: req.context.sourceIp,
+        userAgent: req.context.userAgent,
+        date: new Date(),
     },
   }
   return dynamoDb.put(params).promise()
